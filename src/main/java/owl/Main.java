eae4ef968e59d0a5aab8e35692aa99f7d1bec65d
Jennifer Warrender <jaydchan@gmail.com>
2021-05-20 18:14:48 +0100

    Check for no arg and convert dos file to unix file

--- src/main/java/owl/Main.java
+++ src/main/java/owl/Main.java
@@ -4,28 +4,24 @@ import org.semanticweb.owlapi.formats.OWLXMLDocumentFormat;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-
 // http://syllabus.cs.manchester.ac.uk/pgt/2020/COMP62342/introduction-owl-api-msc.pdf
 public class Main {
     public static void main(String[] args) {
-	
-        try {
-	    // parse argument value
-	    int size = 0;
-	    try {
-		size = Integer.parseInt(args[0]);
-	    } catch (NumberFormatException e) {
-		System.out.println(e.getMessage());
-		System.exit(1);
-	    }
 
-	    // check it is a valid number
-	    if (size <= 0)
-		System.exit(1);
-	    
+	// parse argument value
+	int size = 0;
+	try {
+	    size = Integer.parseInt(args[0]);
+	} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+	    System.out.println(e.getMessage());
+	    System.exit(1);
+	}
+
+	// check it is a valid number
+	if (size <= 0)
+	    System.exit(1);
+
+	try {
             // create an ontology
             OWLOntologyManager man = OWLManager.createOWLOntologyManager();
             OWLOntology o = man.createOntology();
@@ -49,3 +45,4 @@ public class Main {
         }
     }
 }
+
